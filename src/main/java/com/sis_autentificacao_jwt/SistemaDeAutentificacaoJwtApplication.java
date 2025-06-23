88c8f3cad01279d9e54272744a08fee5ae68a0c8
Rodrigo <r.oliveira.95@hotmail.com>
2025-06-23 13:28:04 -0300

    First commit

---
+++ src/main/java/com/sis_autentificacao_jwt/SistemaDeAutentificacaoJwtApplication.java
@@ -0,0 +1,13 @@
+package com.sis_autentificacao_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAutentificacaoJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAutentificacaoJwtApplication.class, args);
+	}
+
+}
